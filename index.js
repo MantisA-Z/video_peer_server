d706a883c03369cbcfaaee675b4d6539c7ee5be9
MantisA-Z <160739281+MantisA-Z@users.noreply.github.com>
2024-09-24 19:47:06 +0530

    Add server files

---
+++ index.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const http = require('http');
+const {PeerServer} = require('peer')
+const cors = require('cors');
+const express = require('express');
+const app = express()
+const {Server} = require('socket.io')
+const server = http.createServer(app)
+const io = new Server(server, {
+    cors: true
+})
+const {v4: uuid} = require('uuid')
+
+const userMap = new Map()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('yo what\'s us')
+})
+
+const peerServer = PeerServer({port: process.env.PEER_SERVER_PORT, path: '/peerjs'})
+peerServer.on('error', (err) => {
+    console.error('Peer error:', err);
+});
+
+
+io.on('connection', (socket) => {
+    console.log(`new socket connection: ${socket.id}`);
+
+    const peerId = uuid();
+    socket.emit('server:join-peer', {peerId})
+
+    const connectedUsers = userMap.get('lobby') || []
+    const socketId = socket.id;
+    userMap.set('lobby', [...connectedUsers, {socketId, peerId}]);
+
+    if(userMap.get('lobby').length === 2){
+        const remotePeerId = userMap.get('lobby')[0].peerId;
+        socket.emit('server:sufficient:users', {remotePeerId})
+    }
+
+    socket.on('disconnect', () => {
+        // Filter out the disconnected user
+        const usersLeft = userMap.get('lobby').filter((user) => user.socketId !== socket.id);
+        
+        // Update userMap with the filtered list
+        userMap.set('lobby', usersLeft);
+        
+        // Emit an event that a user has left (if needed)
+        io.emit('user:left', { socketId }); // Optionally send the socketId of the user that left
+        console.log(`User disconnected. Connected users: ${usersLeft}`);
+    });
+    
+})
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+    console.log(`server started on port: ${port}`)
+})
